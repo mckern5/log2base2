89836db1b874c21fab9bd15ddb999f91b4cbb0dd
Matthew Kern <mattcs321@gmail.com>
2024-12-24 16:34:27 -0500

    add beginning of head

---
+++ src/linkedlist/insertnodeatbeginning.java
@@ -0,0 +1,23 @@
+package linkedlist;
+
+public class insertnodeatbeginning {
+
+	class node{
+		int data;
+		node next;
+	}
+	
+	public class linkedlist {
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	
+}
